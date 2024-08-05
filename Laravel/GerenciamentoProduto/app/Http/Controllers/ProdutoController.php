d975176fc8c31f54c455bba0938d128bdffb9f43
epicestudar <viniciusftza@gmail.com>
2024-08-05 01:46:34 -0300

    configurando o model e controller + criando as views

---
+++ Laravel/GerenciamentoProduto/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produto;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $produtos = Produto::all();
+        return view('produtos.index', compact('produtos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('produtos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'preco' => 'required|decimal'
+        ]);
+
+        Produto::create($request->all());
+
+        return redirect()->route('produtos.index')
+            ->with('success', 'Produto criado com sucesso.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return view('produtos.show', compact('produto'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        return view('produtos.edit', compact('produto'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Produto $produto)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'preco' => 'required|decimal',
+        ]);
+
+        $produto->update($request->all());
+
+        return redirect()->route('produtos.index')
+            ->with('success', 'Produto atualizado com sucesso.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Produto $produto)
+    {
+        $produto->delete();
+
+        return redirect()->route('produtos.index')
+                         ->with('success', 'Produto Deletado com Sucesso.');
+    }
+
+}
--- Laravel/GerenciamentoProduto/routes/web.php
+++ Laravel/GerenciamentoProduto/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('produtos', ProdutoController::class);
